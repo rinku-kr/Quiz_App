bb8dc9d89f236da4fc5995ef6df87a80804408f1
rinku-kr <rk24736599@gmail.com>
2023-11-15 11:45:44 +0530

    adding a stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/quiz_screen.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-              gradient: LinearGradient(colors: [
-            Color.fromARGB(98, 119, 38, 68),
-            Color.fromARGB(97, 92, 15, 55)
-          ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
-          child: const StartScreen(),
-        ),
-      ),
-    ),
-  );
+  runApp(const QuizScreen());
 }
---
+++ lib/quiz_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
+
+class QuizScreen extends StatefulWidget {
+  const QuizScreen({super.key});
+
+  @override
+  State<QuizScreen> createState() {
+    return _QuizScreen();
+  }
+}
+
+class _QuizScreen extends State<QuizScreen> {
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(colors: [
+            Color.fromARGB(98, 119, 38, 68),
+            Color.fromARGB(97, 92, 15, 55)
+          ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+          child: const StartScreen(),
+        ),
+      ),
+    );
+  }
+}
