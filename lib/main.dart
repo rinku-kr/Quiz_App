ac7b691069f3cc5a10d0fe634e0935d3dec31bc0
rinku-kr <rk24736599@gmail.com>
2023-11-15 09:56:48 +0530

    adding image and text-content

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,12 @@ void main() {
     MaterialApp(
       home: Scaffold(
         body: Container(
-          child: StartScreen(),
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(colors: [
+            Color.fromARGB(98, 119, 38, 68),
+            Color.fromARGB(97, 92, 15, 55)
+          ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+          child: const StartScreen(),
         ),
       ),
     ),
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -5,8 +5,32 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(context) {
-    return const Center(
-      child: Text('Start-screen!'),
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 200,
+          ),
+          const SizedBox(
+            height: 80,
+          ),
+          const Text(
+            'EASY WAY TO LEARN FLUTTER!',
+            style: TextStyle(color: Colors.white, fontSize: 22),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          OutlinedButton(
+              onPressed: () {},
+              style: OutlinedButton.styleFrom(
+                foregroundColor: Colors.white,
+              ),
+              child: const Text('Hit Me'))
+        ],
+      ),
     );
   }
 }
