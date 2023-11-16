67d1b1346debeb8c6973e301911c044fb47c5fb5
rinku-kr <rk24736599@gmail.com>
2023-11-16 20:25:30 +0530

    add button style

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -17,7 +17,10 @@ class AnswerButton extends StatelessWidget {
           backgroundColor: const Color.fromARGB(255, 110, 40, 40),
           shape:
               RoundedRectangleBorder(borderRadius: BorderRadius.circular(40))),
-      child: Text(answerText),
+      child: Text(
+        answerText,
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
